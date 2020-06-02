6df83bad24a96cdc09cc7b943a64b3fda8c5336f
xingfuryda <xingfuryda@users.noreply.github.com>
2020-06-02 14:02:17 -0600

    testing package update process

--- src/Layers/BasemapLayer.js
+++ src/Layers/BasemapLayer.js
@@ -310,6 +310,7 @@ function _fetchTilemap (evt) {
             // if necessary, resample a lower zoom
             this.options.maxNativeZoom = newZoom - 1;
             this.options.downsampled = true;
+            debugger;
             map._resetView(map.getCenter(), map.getZoom(), true)
             break;
           }
